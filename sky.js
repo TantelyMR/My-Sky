7a05ff154039934bf5a8bc2507b6b7748c32050d
TantelyMR <tantely.m.ra@gmail.com>
2023-07-25 12:31:49 -0400

    First updatge to responsivity -Skills and Tech Section

--- sky.js
+++ sky.js
@@ -4,58 +4,46 @@ const sideBar = document.querySelector('#side-bar');
 const header = document.querySelector('header');
 const technology = document.querySelector('#technology');
 
-function navBackgroundColor () {
-  if (window.scrollY > header.offsetHeight) {
-    topNav.classList.add("bordered");
-  } else {
-    topNav.classList.remove("bordered");
+function navBackgroundColor() {
+  topNav.classList.toggle('bordered', window.scrollY > header.offsetHeight);
+}
+
+function sideColorChange() {
+  sideBar.style.color = (window.scrollY < header.offsetHeight / 2) && (window.innerWidth > 680) ? 'black' : 'white';
+}
+
+function showFigcaption(event) {
+  const figcaption = event.currentTarget.querySelector('figcaption');
+  if (figcaption) {
+    figcaption.style.opacity = '100%';
   }
 }
 
-/*
-function horizontalWheelScroll(event) {
-
-  if (window.getComputedStyle(this).overflow === 'auto') {
-    event.preventDefault();
-    this.scrollLeft += event.deltaY;
+function hideFigcaption(event) {
+  const figcaption = event.currentTarget.querySelector('figcaption');
+  if (figcaption) {
+    figcaption.style.opacity = '0%';
   }
 }
 
-technology.addEventListener('wheel', horizontalWheelScroll);
-
-*/
-
+window.addEventListener('scroll', sideColorChange);
 window.addEventListener('scroll', navBackgroundColor);
 
 // Wait for the DOM content to be fully loaded before running the script
-document.addEventListener("DOMContentLoaded", function () {
-  // Step 1: Get the skill section
-  const skillSection = document.getElementById("skill");
+document.addEventListener('DOMContentLoaded', function () {
+  const skillSection = document.getElementById('skill');
 
   if (!skillSection) {
-    console.error("Skill section not found!");
+    console.error('Skill section not found!');
     return;
   }
-  const skillFigures = skillSection.querySelectorAll("figure");
+  const skillFigures = skillSection.querySelectorAll('figure');
 
   skillFigures.forEach((figure) => {
-    figure.addEventListener("mouseover", showFigcaption);
-    figure.addEventListener("focus", showFigcaption);
-    figure.addEventListener("mouseout", hideFigcaption);
-    figure.addEventListener("blur", hideFigcaption);
+    figure.addEventListener('mouseover', showFigcaption);
+    figure.addEventListener('focus', showFigcaption);
+    figure.addEventListener('mouseout', hideFigcaption);
+    figure.addEventListener('blur', hideFigcaption);
   });
-
-  function showFigcaption(event) {
-    const figcaption = event.currentTarget.querySelector("figcaption");
-    if (figcaption) {
-      figcaption.style.opacity = "100%";
-    }
-  }
-
-  function hideFigcaption(event) {
-    const figcaption = event.currentTarget.querySelector("figcaption");
-    if (figcaption) {
-      figcaption.style.opacity = "0%";
-    }
-  }
-});
+}
+);
